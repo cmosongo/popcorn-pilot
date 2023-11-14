bc990800c41293c43335c0eb2536711ffa6fdd12
cmosongo <charles.mosongo@strathmore.edu>
2023-11-14 06:58:10 +0300

    final changes

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # popcorn-pilot
-Movie Recommendation System
+popcorn-pilot is a movie recommendation system based of a mock challenge.
+ 
+The purpose of the challenge is to develop a system providing more accurate and personalized movie recommendations.
+
+The solution will not only increase user engagement but also improve subscription renewals, making a significant impact in the world of entertainment.
+
+## Challenge Breakdown
+Project Description:
+Your task involves creating a proof-of-concept movie recommendation system. You will work with pre-existing user data and a current movie catalog to deliver personalized movie recommendations.
+
+Task Description:
+Data Analysis:
+- Start by cleaning the provided dummy data. Attached
+- Dive into the cleaned data to uncover valuable insights into user preferences, such as favorite genres, average ratings, and more.
+
+Algorithm Development:
+- Develop a movie recommendation algorithm that takes into account user ratings.
+- Implement robust measures to handle irregular or incomplete data.
+- Create a fallback system that recommends popular movies if a user's data doesn't provide enough information.
+
+User Interface:
+- Design a simple Command Line Interface (CLI) where a user can input their name to receive personalized movie recommendations.
+- Present these recommendations in a user-friendly format.
+
+Submission Details:
+Your solutions should be submitted by 14th November at 8:00 AM.
+Please ensure that the code is hosted on a GitHub repository, and is submitted using the following link: GitHub Repository Submission Link.
+
+
---
+++ src/main.py
@@ -0,0 +1,89 @@
+# Import necessary libraries/modules
+#general libs
+import os
+import time
+
+#computing libs
+import numpy as np
+import pandas as pd
+
+#ml model libs
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.impute import SimpleImputer
+
+
+# plotting libs
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# load data
+file_path = "../data/internal/data.csv"
+
+data = pd.read_csv(file_path,
+                  sep=",")
+
+#movies
+movies = data['Movie'].unique()
+
+#average ratings
+average_ratings = data.groupby('Movie')['Rating'].mean().reset_index()
+
+# Collaborative Filtering Algorithm
+user_movie_matrix = data.pivot_table(index='User', columns='Movie', values='Rating', fill_value=0)
+
+cosine_sim = cosine_similarity(user_movie_matrix.T)
+data_cos = pd.DataFrame(cosine_sim, columns=movies, index=movies)
+
+
+def get_top_indices(df, n):
+    # Flatten the DataFrame and sort the values
+    flatten_values = df.values.flatten() 
+    sorted_values = sorted(flatten_values, reverse=True)
+    
+    # Get the unique indices of the n largest values
+    unique_indices = df[df.isin(sorted_values[:n] ).values].dropna().index.unique()
+    return unique_indices
+
+def recommend_movies(user):
+    movie_ratings = user_movie_matrix.loc[user]
+    watched_list = movie_ratings[movie_ratings > 0]
+    
+    mod_data_cos = data_cos.drop(watched_list.index, axis = 1)
+    
+    recc_list = []
+    for index, value in watched_list.items():
+        weight = value/watched_list.sum()
+        recc_list.append(mod_data_cos.loc[index]*(1+weight))
+        
+        final_df = pd.concat(recc_list, axis=1)
+    return get_top_indices(final_df, 3)
+
+def main():
+    while True:
+        user_name = input("Please enter your name: ").upper()
+
+        # Check if the user exists in the data
+        if user_name not in user_movie_matrix.index:
+            print("User not found !!!")
+            time.sleep(0.1)
+            print("Recommending popular movies.")
+            time.sleep(1)
+            popular_movies = average_ratings.sort_values(by='Rating', ascending=False)['Movie']
+            recommendations = list(popular_movies.head(3))
+            print(recommendations)
+
+        else :
+            recommendations = list(recommend_movies(user_name))
+            print(f"Recommended movies for {user_name}")
+            time.sleep(1)
+            print(recommendations)
+        
+
+# Check if the script is run as the main program
+if __name__ == "__main__":
+    try:
+        # Call the main function
+        main()
+    except KeyboardInterrupt:
+        print('Exiting ...')
+
